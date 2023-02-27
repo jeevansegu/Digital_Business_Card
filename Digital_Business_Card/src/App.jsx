4153a8e8e34777ff589e9c60becd4cc522661ac7
jeevansegu <jeevansegu03@gmail.com>
2023-02-27 13:25:55 +0530

    added folders

---
+++ Digital_Business_Card/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Info from '../components/Info'
+import Footer from '../components/Footer'
+import About from '../components/About'
+import Interests from '../components/Interests'
+
+function App() { 
+  return (
+    <div className="App">
+      <Info />
+      <About />
+      <Interests />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
